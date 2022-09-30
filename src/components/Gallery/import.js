c7bad61c440dddecc004d984a898e4e2b9e7b6c9
davisLjr <davisprojectjr@gmail.com>
2022-09-30 17:27:15 -0300

    fix: solved size img

---
+++ src/components/Gallery/import.js
@@ -0,0 +1,129 @@
+import Img3 from '../../resources/img/images/3.jpg'
+import Img3L from '../../resources/img/resize/3.jpg'
+import Img4 from '../../resources/img/images/4.jpg'
+import Img4L from '../../resources/img/resize/4.jpg'
+import Img5 from '../../resources/img/images/5.jpg'
+import Img5L from '../../resources/img/resize/5.jpg'
+import Img6 from '../../resources/img/images/6.jpg'
+import Img6L from '../../resources/img/resize/6.jpg'
+import Img7 from '../../resources/img/images/7.jpg'
+import Img7L from '../../resources/img/resize/7.jpg'
+import Img8 from '../../resources/img/images/8.jpg'
+import Img8L from '../../resources/img/resize/8.jpg'
+import Img9 from '../../resources/img/images/9.jpg'
+import Img9L from '../../resources/img/resize/9.jpg'
+import Img11 from '../../resources/img/images/11.jpg'
+import Img11L from '../../resources/img/resize/11.jpg'
+import Img12 from '../../resources/img/images/12.jpg'
+import Img12L from '../../resources/img/resize/12.jpg'
+import Img13 from '../../resources/img/images/13.jpg'
+import Img13L from '../../resources/img/resize/13.jpg'
+import Img14 from '../../resources/img/images/14.jpg'
+import Img14L from '../../resources/img/resize/14.jpg'
+import Img15 from '../../resources/img/images/15.jpg'
+import Img15L from '../../resources/img/resize/15.jpg'
+import Img16 from '../../resources/img/images/16.jpg'
+import Img16L from '../../resources/img/resize/16.jpg'
+import Img17 from '../../resources/img/images/17.jpg'
+import Img17L from '../../resources/img/resize/17.jpg'
+import Img18 from '../../resources/img/images/18.jpg'
+import Img18L from '../../resources/img/resize/18.jpg'
+import Img19 from '../../resources/img/images/19.jpg'
+import Img19L from '../../resources/img/resize/19.jpg'
+import Img20 from '../../resources/img/images/20.jpg'
+import Img20L from '../../resources/img/resize/20.jpg'
+import Img21 from '../../resources/img/images/21.jpg'
+import Img21L from '../../resources/img/resize/21.jpg'
+
+export const data = [
+{
+id: 3,
+imgSrc: Img3L,
+thumbnailSrc: Img3,
+},
+{
+id: 4,
+imgSrc: Img4L,
+thumbnailSrc: Img4,
+},
+{
+id: 5,
+imgSrc: Img5L,
+thumbnailSrc: Img5,
+},
+{
+id: 6,
+imgSrc: Img6L,
+thumbnailSrc: Img6,
+},
+{
+id: 7,
+imgSrc: Img7L,
+thumbnailSrc: Img7,
+},
+{
+id: 8,
+imgSrc: Img8L,
+thumbnailSrc: Img8,
+},
+{
+id: 9,
+imgSrc: Img9L,
+thumbnailSrc: Img9,
+},
+{
+id: 11,
+imgSrc: Img11L,
+thumbnailSrc: Img11,
+},
+{
+id: 12,
+imgSrc: Img12L,
+thumbnailSrc: Img12,
+},
+{
+id: 13,
+imgSrc: Img13L,
+thumbnailSrc: Img13,
+},
+{
+id: 14,
+imgSrc: Img14L,
+thumbnailSrc: Img14,
+},
+{
+id: 15,
+imgSrc: Img15L,
+thumbnailSrc: Img15,
+},
+{
+id: 16,
+imgSrc: Img16L,
+thumbnailSrc: Img16,
+},
+{
+id: 17,
+imgSrc: Img17L,
+thumbnailSrc: Img17,
+},
+{
+id: 18,
+imgSrc: Img18L,
+thumbnailSrc: Img18,
+},
+{
+id: 19,
+imgSrc: Img19L,
+thumbnailSrc: Img19,
+},
+{
+id: 20,
+imgSrc: Img20L,
+thumbnailSrc: Img20,
+},
+{
+id: 21,
+imgSrc: Img21L,
+thumbnailSrc: Img21,
+},
+];
